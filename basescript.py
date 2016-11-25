a48588c6758c5fc8d3a9ce2f6a4497c304369a9e
Ashkon Honardoost <ashkon91@gmail.com>
2016-11-25 14:45:52 -0800

    Updated Script

--- basescript.py
+++ basescript.py
@@ -2,31 +2,53 @@ from urllib2 import urlopen
 import json
 import requests
 import pprint
+import csv
+import sys
+
+f = open(sys.argv[1], 'wt')
 
 pp = pprint.PrettyPrinter(indent=4)
 
 url = 'http://ws.audioscrobbler.com/2.0/'
+api_key = '361fad39798b9291b9cb37f447c829cc'
 
-data = urlopen(url)
-response = data.read()
-vals = json.loads(response)
-songs = []
-def songsPerPage(url, pagenum):
-    song = []
+def songsPerPage(url, pagenum, user, api_key):
+    songs = []
     payload = {'method':'user.getrecenttracks', 'user':user, 'api_key':api_key, 'limit':'200', 'page':pagenum, 'format':'json'}
     response = requests.get(url, data=payload )
     json_data = json.loads(response.text)
-
     for track in json_data['recenttracks']['track']:
+        song = []
+        song.append(track['artist']['#text'])
         song.append(track['name'])
-    return song
+        song.append(track['date']['#text'])
+        songs.append(song)
+    return songs
 
-testpayload = { 'method':'user.getrecenttracks', 'user':'ashkon91', 'api_key': '361fad39798b9291b9cb37f447c829cc', 'format':'json'}
+def writeToCSV(songs,f):
+    try:
+        writer = csv.writer(f)
+        writer.writerow( ("ARTIST", "SONG", "DATE") )
+        for song in songs:
+            print song
+            writer.writerow( (song[0].encode("utf-8"), song[1].encode("utf-8"), song[2].encode("utf-8") ))    
+    finally:
+        f.close()
 
-responsedata = requests.get(url, params = testpayload)
+songs = []
+
+payload = { 'method':'user.getrecenttracks', 'limit':'200','user':'Ashkon91', 'api_key': '361fad39798b9291b9cb37f447c829cc', 'format':'json'}
+
+print "ENTER"
+
+responsedata = requests.get(url, params = payload)
 jsond = json.loads(responsedata.text)
 totalPages = int(jsond['recenttracks']['@attr']['totalPages'])
 print totalPages
-
 for i in range(totalPages):
-    songs += songsPerPage(urltracks, str(i+1))
+    print i,len(songs)
+    songs += songsPerPage(url, str(i+1), 'Ashkon91', api_key )
+
+print "END"
+writeToCSV(songs,f)
+
